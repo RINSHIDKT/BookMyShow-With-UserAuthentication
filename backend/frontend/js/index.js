b2ceae4de43a9853c29dbdf1dfdabc997f0f0e8a
RINSHIDKT <rinshidkt6@gmail.com>
2023-11-09 19:26:47 +0530

    added

--- backend/frontend/js/index.js
+++ backend/frontend/js/index.js
@@ -46,3 +46,23 @@ function del(){
 }
 
 getMovies();
+
+async function SearchFunction() {
+  let inp=document.getElementById("Search-bar").value
+  const Movie = await fetch("http://localhost:3004/BookMyShow/movies");
+  const data = await Movie.json();
+  s = "";
+  data.filter((dt) => {
+
+    if(dt.Movie_Title.startsWith(inp)){
+      s += `<div class="card">
+    <a href="./pages/Details.html?id=${dt._id}"><img class="card-img" src="${dt.Movie_Poster}" alt=""></a>
+    <div class="discription">
+        <div class="film"><span class="film-name">${dt.Movie_Title}</span></div>
+        <div><span class="genre">${dt.Genre}</span></div>
+    </div>
+</div>`;
+    }
+  });
+  document.getElementById("show").innerHTML = s;
+}
